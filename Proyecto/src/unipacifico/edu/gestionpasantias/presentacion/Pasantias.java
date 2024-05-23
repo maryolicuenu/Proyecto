69852cf380b75a15a608aced839fe611ce8a804e
Javier <Javier@Javier-pc>
2024-05-23 16:56:25 -0500

    interfaz grafica

--- Proyecto/src/unipacifico/edu/gestionpasantias/presentacion/Pasantias.java
+++ Proyecto/src/unipacifico/edu/gestionpasantias/presentacion/Pasantias.java
@@ -211,6 +211,7 @@ public class Pasantias extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         
+       
     }//GEN-LAST:event_jButton1ActionPerformed
 
 /**
